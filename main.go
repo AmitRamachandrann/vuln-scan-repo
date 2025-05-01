e820d44de396caf0624b96e83e457c3fbfa35260
amitRamachandrannn <aramachandran@cloudbees.com>
2025-05-01 10:41:03 +0530

    aws key test for gitleaks

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Payload struct {
 const password = "super_secret"
 
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	apiKey := "my-secret-key"
+	apiKey := "ASIAY34FZKBOKMUTVV7A"
 	name := p.ByName("name")
 	payload := Payload{
 		Message:  "Hello " + name,
